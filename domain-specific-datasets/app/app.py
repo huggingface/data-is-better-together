0bc9eef7801e20b299a525b7be399023469fa4a6
Ben Burtenshaw <ben@argilla.io>
2024-04-16 20:25:34 +0200

    enhancement: refactor app for simplicity and easy use

--- domain-specific-datasets/app/app.py
+++ domain-specific-datasets/app/app.py
@@ -19,14 +19,17 @@ from defaults import (
 )
 from pipeline import serialize_pipeline
 
-st.markdown("# 🧑‍🌾 Domain Data Grower")
-st.markdown("## 🌱 Create a dataset seed for aligning models to a specific domain")
-st.markdown(
-    "This app helps you create a dataset seed for building diverse domain-specific datasets for aligning models."
-)
-st.markdown(
-    "Alignment datasets are used to fine-tune models to a specific domain or task, but as yet, there's a shortage of diverse datasets for this purpose."
-)
+################################################################################
+# Introduction and Project Setup
+################################################################################
+
+introduction = """# 🧑‍🌾 Domain Data Grower
+## 🌱 Create a dataset seed for aligning models to a specific domain
+
+This app helps you create a dataset seed for building diverse domain-specific datasets for aligning models.
+Alignment datasets are used to fine-tune models to a specific domain or task, but as yet, there's a shortage of diverse datasets for this purpose.
+"""
+st.markdown(introduction)
 
 project_name = st.text_input("Project Name", DEFAULT_DOMAIN)
 domain = st.text_input("Domain", DEFAULT_DOMAIN)
@@ -35,35 +38,104 @@ hub_token = st.text_input("Hub Token", type="password")
 argilla_url = st.text_input("Argilla API URL", "https://argilla-farming.hf.space")
 argilla_api_key = st.text_input("Argilla API Key", "owner.apikey")
 
+################################################################################
+# Domain Expert Section
+################################################################################
+st.divider()
 st.header("👩🏼‍🔬 Domain Expert")
-st.header("Define the domain expertise that you want to train a language model on.")
+st.text("Define the domain expertise that you want to train a language model")
+st.info(
+    "A domain expert is a person who is an expert in a particular field or area. For example, a domain expert in farming would be someone who has extensive knowledge and experience in farming and agriculture."
+)
+
 domain_expert_prompt = st.text_area(
-    label="Domain Expertise",
+    label="Domain Expert Definition",
     value=DEFAULT_SYSTEM_PROMPT,
-)
-st.header("👯️ Domain Perspectives")
-st.markdown(
-    "Perspectives are different viewpoints or angles from which a domain can be viewed. For example, the domain of farming can be viewed from the perspective of a commercial farmer or an independent family farmer."
+    height=200,
 )
 
-perspectives = [
-    st.text_input(f"Domain Perspective {n}", value=DEFAULT_PERSPECTIVES[n])
-    for n in range(N_PERSPECTIVES)
-]
+################################################################################
+# Domain Perspectives
+################################################################################
 
-st.header("🧺️ Domain Topics")
-st.markdown(
-    "Topics are the main themes or subjects that are relevant to the domain. For example, the domain of farming can have topics like soil health, crop rotation, or livestock management."
+st.divider()
+st.header("🔍 Domain Perspectives")
+st.text("Define the different perspectives from which the domain can be viewed")
+st.info(
+    """
+Perspectives are different viewpoints or angles from which a domain can be viewed. 
+For example, the domain of farming can be viewed from the perspective of a commercial 
+farmer or an independent family farmer."""
 )
-topics = [
-    st.text_input(f"Domain Topic {n}", value=DEFAULT_TOPICS[n]) for n in range(N_TOPICS)
-]
 
+perspectives = st.session_state.get(
+    "perspectives",
+    [st.text_input(f"Domain Perspective 0", value=DEFAULT_PERSPECTIVES[0])],
+)
+new_perspective = st.button("Add New Perspective")
+
+if new_perspective:
+    n = len(perspectives)
+    value = DEFAULT_PERSPECTIVES[n] if n < N_PERSPECTIVES else ""
+    perspectives.append(st.text_input(f"Domain Perspective {n}", value=""))
+    st.session_state["perspectives"] = perspectives
+
+
+################################################################################
+# Domain Topics
+################################################################################
+
+st.divider()
+st.header("🕸️ Domain Topics")
+st.text("Define the main themes or subjects that are relevant to the domain")
+st.info(
+    """Topics are the main themes or subjects that are relevant to the domain. For example, the domain of farming can have topics like soil health, crop rotation, or livestock management."""
+)
+topics = st.session_state.get(
+    "topics", [st.text_input(f"Domain Topic 0", value=DEFAULT_TOPICS[0])]
+)
+new_topic = st.button("Add New Topic")
+
+if new_topic:
+    n = len(topics)
+    value = DEFAULT_TOPICS[n] if n < N_TOPICS else ""
+    topics.append(st.text_input(f"Domain Topic {n}", value=value))
+    st.session_state["topics"] = topics
+
+
+################################################################################
+# Examples Section
+################################################################################
+
+st.divider()
 st.header("📚 Examples")
+st.text(
+    "Add high-quality questions and answers that can be used to generate synthetic data"
+)
+st.info(
+    """
+Examples are high-quality questions and answers that can be used to generate 
+synthetic data for the domain. These examples will be used to train the language model
+to generate questions and answers.
+"""
+)
 
-questions_answers = []
+questions_answers = st.session_state.get(
+    "questions_answers",
+    [
+        (
+            st.text_area(
+                "Question", key="question_0", value=DEFAULT_EXAMPLES[0]["question"]
+            ),
+            st.text_area("Answer", key="answer_0", value=DEFAULT_EXAMPLES[0]["answer"]),
+        )
+    ],
+)
 
-for n in range(N_EXAMPLES):
+new_question = st.button("Add New Example")
+
+if new_question:
+    n = len(questions_answers)
     default_question, default_answer = DEFAULT_EXAMPLES[n].values()
     st.subheader(f"Example {n + 1}")
     if st.button("Generate New Answer", key=f"generate_{n}"):
@@ -71,9 +143,22 @@ for n in range(N_EXAMPLES):
     _question = st.text_area("Question", key=f"question_{n}", value=default_question)
     _answer = st.text_area("Answer", key=f"answer_{n}", value=default_answer)
     questions_answers.append((_question, _answer))
+    st.session_state["questions_answers"] = questions_answers
 
+################################################################################
+# Create Dataset Seed
+################################################################################
 
+create_text = """
+# 🤗 Create Dataset on the Hub
+Create a dataset seed and push it to the Hub to grow a domain-specific dataset.
+"""
+st.markdown(create_text)
 if st.button("Create Dataset Seed :seedling:"):
+    ############################################################
+    # Create Dataset Seed and serialize pipeline
+    ############################################################
+
     perspectives = list(filter(None, perspectives))
     topics = list(filter(None, topics))
     examples = [{"question": q, "answer": a} for q, a in questions_answers]
@@ -99,6 +184,10 @@ if st.button("Create Dataset Seed :seedling:"):
         domain_expert_prompt=domain_expert_prompt or DEFAULT_SYSTEM_PROMPT,
     )
 
+    ############################################################
+    # Setup Dataset on the Hub
+    ############################################################
+
     push_dataset_to_hub(
         domain_seed_data_path=SEED_DATA_PATH,
         project_name=project_name,
@@ -108,8 +197,8 @@ if st.button("Create Dataset Seed :seedling:"):
         pipeline_path=PIPELINE_PATH,
     )
 
-    st.markdown(
-        f"# :seedling: Dataset is ready to grow \nDataset seed created and pushed to the Hub. Check it out [here](https://huggingface.co/{hub_username}/{project_name})"
+    st.success(
+        f"Dataset seed created and pushed to the Hub. Check it out [here](https://huggingface.co/{hub_username}/{project_name})"
     )
 
     instructions = f"""
